c20b31f4c38e9def57193b485dac06bcfa33d448
John Bender <john.m.bender@gmail.com>
2015-01-31 16:05:19 -0800

    basic array match

--- homework/pm/match.js
+++ homework/pm/match.js
@@ -1,14 +1,47 @@
-window._ = "placeholder";
+window._ = "wildcard";
 
 function isValue(e) {
-  if( typeof e === "number" ){
+  if( typeof e === "number" || typeof e === "string" ){
     return true;
   }
 }
 
+function matchArray(value, check, bindings) {
+  // if both are empty return the bindings
+  if( !value.length && !check.length ){
+    return bindings;
+  }
+
+  // if the check still has elements but value doesn't, vice versa
+  if( value.length !== check.length ){
+    return false;
+  }
+
+  var v, c;
+
+  v = value.shift();
+  c = check.shift();
+
+
+  // wildcard binding
+  if( isValue(v) && c === window._ ) {
+    bindings.push(v);
+  } else if( isValue(v) && c !== v ){
+    return false;
+  }
+
+  // nested array should recurse with nested arrays and append the bindings
+  if( Array.isArray(v) ){
+    bindings.concat(matchArray(v, c, bindings));
+  }
+
+  // otherwise we assume a match and proceed
+  return matchArray(value, check, bindings);
+}
+
 function match(value /* , pat1, fun1, pat2, fun2, ... */) {
   var clauses = [].slice.call(arguments, 1);
-  var check, exec;
+  var check, exec, bindings;
 
   while( clauses.length ){
     check = clauses.shift();
@@ -26,7 +59,14 @@ function match(value /* , pat1, fun1, pat2, fun2, ... */) {
       return exec(value);
     }
 
-    // match
-    // invoke with
+    if( value.length ) {
+      // otherwise assume array
+      bindings = matchArray(value, check, []);
+
+      // if false wasn't returned use the bindings
+      if( bindings ){
+        return exec.apply(window, bindings);
+      }
+    }
   }
 }
